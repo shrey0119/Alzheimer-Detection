b3e9019122d34b1dcfe18c887180c0a6ff8b3b0e
shreya <185189341+shreya0119@users.noreply.github.com>
2026-04-03 09:01:49 +0000

    Add Colab notebook; fix duplicate browser tab on app startup

--- app.py
+++ app.py
@@ -56,7 +56,4 @@ def predict():
 
 if __name__ == '__main__':
     threading.Timer(1.5, lambda: webbrowser.open("http://127.0.0.1:5000")).start()
-    app.run(debug=True)
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True,use_reloader=False)
